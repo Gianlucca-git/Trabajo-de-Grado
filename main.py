ecc3a2efc757db07b31406ff81d01a04ee0f05c0
Gianlucca-Git <gianlucca.aguado@correounivalle.edu.co>
2021-04-14 20:35:22 -0500

    Commit 1: Creacion del menu

---
+++ main.py
@@ -0,0 +1,11 @@
+import essentia
+import IPython.display as ipd
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy
+from essentia.standard import *
+import warnings 
+warnings.filterwarnings('ignore')
+
+
+print("hola")
